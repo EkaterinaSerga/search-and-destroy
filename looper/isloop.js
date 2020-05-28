be0a4e5f8be57139b8a3f6d643b5dfefeb8a0e6f
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-05-28 17:57:53 -0400

    First commit for the Loopy Linked List exercise

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,20 @@
-'use strict';
+"use strict";
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  if (linkedlist.tail.next === null) {
+    return false;
+  }
 
+  let node = linkedlist.head;
+  if (!node.previous) {
+    node = node.next;
+  } else {
+    if (node.previous.next.value !== node.value) {
+      return true;
+    }
+  }
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +24,20 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
+
+//ASSUMPTIONS:
+// - linked list is an object
+// - the linked list has HEAD & TAIL
+// - HEAD's previous is ALWAYS null;
+// - the head and tail are nodes which have VALUE, PREVIOUS, NEXT
+// - the value of each node is unique
+// - DO NOT mutate any nodes on the list
+
+//EDGE CASES:
+// - IF tail's next !== null => return TRUE
+// - IF tail's next === null => return FALSE
+
+//APPROACH:
+// - If node.previous.next !== node => THERE IS A LOOP (only works for doubly lists)
+// - to check that: start with head then moving to it's next node until you reach the tail
